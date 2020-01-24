9376f32d698e5296dfe4ecf59e973a4381f5ed71
Matthew Wing <wingm1@montclair.edu>
2020-01-24 15:14:00 -0500

    Updates

---
+++ precache-manifest.e42e58046704ea416897fddbc0a19660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48b047f56c14781db773",
+    "url": "/static/css/main.fce7eaee.chunk.css"
+  },
+  {
+    "revision": "48b047f56c14781db773",
+    "url": "/static/js/main.48b047f5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2fc23ab60ee73a40523e765018cbf25",
+    "url": "/index.html"
+  }
+];
