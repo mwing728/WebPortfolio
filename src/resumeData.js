d5e61decabebc1a711e53dac1083b7e33cf604cc
Matt3 <mwing@corp.lawyer.com>
2024-05-13 23:11:25 -0400

    making new changes to my web portfolio Mattw

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,67 +1,67 @@
 let resumeData = {
-    // "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Matthew Wing",
-    "linkedinId":"Your LinkedIn Id",
-    "skills": ["Javascript", "PHP", "HTML5", "CSS3", "Linux", "jQuery", "MySQL"],
-    "roleDescription": "Passionate Software Developer Eager To Learn New Technologies",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/matthew-wing-494b1b139/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/mwing728",
-          "className":"fa fa-github"
-        },
-      ],
-    "aboutme":" Enthusiastic software developer motivated to build applications using PHP, JavaScript, and MySQL.  Determined to learn new languages to improve my skills and increase my knowledge in the software development field. Receiving tips and advice from mentors has helped myself to write more clean code.",
-    "website":"https://rbhatia46.github.io",
-    "work":[
+  // "imagebaseurl":"https://rbhatia46.github.io/",
+  "name": "Matthew Wing",
+  "linkedinId":"Your LinkedIn Id",
+  "skills": ["Javascript", "PHP", "Python", "HTML", "CSS", "Linux", "jQuery", "MySQL"],
+  "roleDescription": "Passionate Software Developer Eager To Learn New Technologies",
+  "socialLinks":[
       {
-        "CompanyName":"Lawyer.com",
-        "CompanyLogo": "images/lawyercom-logo.jfif",
-        "specialization":"Full Stack Developer",
-        "MonthStarted":"Feb 2020 - ",
-        "YearOfLeaving":"Present",
-        "Achievements": ["Maintain the business Softphone Platform through Javascript and PHP", "Use Twilio API (PHP/Javascript) to connect our Sales Representatives to Consumers and Lawyers.", "Support the platform using Pusher API to receive real time updates/notifications", "Build/Debug code on the LAMP stack (Linux, Apache, MySQL, PHP)"]
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/matthew-wing-494b1b139/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/mwing728",
+        "className":"fa fa-github"
       },
     ],
-    "portfolio":[
-      {
-        "name":"ToDoList",
-        "title":"webapp",
-        "imgurl":"images/portfolio/to-do-list1.jpg",
-        "demo":"https://mwing728.github.io/ToDoList/",
-        "githubLink":"https://github.com/mwing728/ToDoList",
-        "description" : "- This is a simple todo list app that uses the state to keep track of the items in the list. Instead of using a class component it only uses functional components and manipulates the state via hooks. "
-      },
-      {
-        "name":"project2",
-        "title":"mobileapp",
-        "imgurl":"images/portfolio/farmerboy.jpg",
-        "demo":"#",
-        "githubLink":"#",
-        "description":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-      },
-      {
-        "name":"project3",
-        "title":"mobileapp",  
-        "imgurl":"images/portfolio/origami.jpg",
-        "demo":"#",
-        "githubLink":"#",
-        "description":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-      },
-      {
-        "name":"project4",
-        "title":"mobileapp",
-        "imgurl":"images/portfolio/retrocam.jpg",
-        "demo":"#",
-        "githubLink":"#",
-        "description":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "aboutme":" Enthusiastic software developer motivated to build applications using PHP, JavaScript, and MySQL.  Determined to learn new languages to improve my skills and increase my knowledge in the software development field. Receiving tips and advice from mentors has helped myself to write more clean code.<br> Actively relearning Python and React to broaden my skill set and deepen my expertise.",
+  "website":"https://rbhatia46.github.io",
+  "work":[
+    {
+      "CompanyName":"Lawyer.com",
+      "CompanyLogo": "images/lawyercom-logo.jfif",
+      "specialization":"Full Stack Developer",
+      "MonthStarted":"Feb 2020 - ",
+      "YearOfLeaving":"Present",
+      "Achievements": ["Proficient in developing and troubleshooting code within the LAMP stack (Linux, Apache, MySQL, PHP) environment", "Accomplished in crafting responsive front-end interfaces using HTML, CSS, JavaScript, and jQuery, with seamless integration into MySQL databases through PHP PDO connections.", "Leveraged Twilio API (PHP/Javascript) to facilitate bidirectional communication between Twilio and applications, enabling seamless calling capabilities for both consumers and legal professionals.", "Implemented real-time updates and notifications in applications using the Pusher API (JavaScript).", "Demonstrated a track record of creating efficient, scalable, and resilient products in alignment with strategic directives set by management."]
+    },
+  ],
+  "portfolio":[
+    {
+      "name":"ToDoList",
+      "title":"webapp",
+      "imgurl":"images/portfolio/to-do-list1.jpg",
+      "demo":"https://mwing728.github.io/ToDoList/",
+      "githubLink":"https://github.com/mwing728/ToDoList",
+      "description" : "- This is a simple todo list app that uses the state to keep track of the items in the list. Instead of using a class component it only uses functional components and manipulates the state via hooks. "
+    },
+    {
+      "name":"Bear With Me",
+      "title":"webapp",
+      "imgurl":"images/portfolio/cute-bear-love.jpg",
+      "demo":"https://mwing728.github.io/bear-with-me/",
+      "githubLink":"https://github.com/mwing728/bear-with-me",
+      "description":"My girlfriend wanted me to write her something and she saw something similar to this on instagram. So this is my attempt at a replication of it in Angular"
+    },
+    {
+      "name":"project3",
+      "title":"mobileapp",  
+      "imgurl":"images/portfolio/origami.jpg",
+      "demo":"#",
+      "githubLink":"#",
+      "description":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    },
+    {
+      "name":"project4",
+      "title":"mobileapp",
+      "imgurl":"images/portfolio/retrocam.jpg",
+      "demo":"#",
+      "githubLink":"#",
+      "description":"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    }
+  ]
+}
+
+export default resumeData
